b2a57f5e4cddd15c25a294aeae20c53bcdec7fd2
sanober riaz <sanober@sanober.me>
2018-01-15 02:22:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,8 @@ function forLoop(array){
   for (var i = 0; i < 25; i++) {
     var s = `I am ${i} strange loop.`;
     array.push(s);
-    array;
+  }
+  return array;
   }
 
 function whileLoop(n){
