8f47338bc13cce1948557b9f3a252bf7970cf9c9
sanober riaz <sanober@sanober.me>
2018-01-15 02:25:56 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array){
+  for (var i = 0; i < 25; i++) {
+    var s = `I am ${i} strange loop.`;
+    array.push(s);
+  }
+  return array;
+  }
+
+function whileLoop(n){
+  while (n > 0) {
+    console.log(n);
+    --n;
+  }
+  return "done";
+}
+
+function doWhileLoop(array){
+do {
+  array.pop();
+  return array;
+} while (maybeTrue()|| array.length >=1);
+}
